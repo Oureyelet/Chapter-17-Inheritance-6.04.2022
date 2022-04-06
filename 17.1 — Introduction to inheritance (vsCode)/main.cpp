c5f33be3af3c9b849d01754fe37707c2754b509e
Mateusz <oureyelet@gmail.com>
2022-04-06 06:31:43 +0100

    17.1 — Introduction to inheritance (vsCode)

--- 17.1 — Introduction to inheritance (vsCode)/main.cpp
+++ 17.1 — Introduction to inheritance (vsCode)/main.cpp
@@ -1,8 +1,44 @@
 #include <iostream>
+#include <string>
+
+class Fruit
+{
+private:
+    std::string m_name{};
+    std::string m_color{};
+    double m_size{};
+
+public:
+    Fruit(std::string name, std::string color, double size)
+        : m_name{ name }, m_color{ color }, m_size{ size }
+    {
+    }
+
+    void getInfo()
+    {
+        std::cout << m_name << " has a " << m_color << " color and " << m_size << " size\n";
+    }
+
+    friend std::ostream& operator<<(std::ostream& out, const Fruit& fruit)
+    {
+        out << fruit.m_name << " has a " << fruit.m_color << " color and " << fruit.m_size << " size\n";
+        return out;
+    }
+
+};
 
 int main()
 {
     std::cout << "Hello World\n";
+
+    Fruit kiwi{"Kiwi", "brown", 2.2};
+
+    std::cout << kiwi;
+
+    //Hierarchies:
+
+    //A look ahead:
+
 
     return 0;
 }
