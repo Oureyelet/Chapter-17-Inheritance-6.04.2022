75940cdbceeaaefd8f872c936dde818f90df0f82
Mateusz <oureyelet@gmail.com>
2022-04-15 14:18:57 +0100

    17.x — Chapter 17 comprehensive quiz (vsCode)

--- 17.x — Chapter 17 comprehensive quiz (vsCode)/question3.cpp
+++ 17.x — Chapter 17 comprehensive quiz (vsCode)/question3.cpp
@@ -1,69 +1,110 @@
 #include <iostream>
 #include <string>
+#include <string_view> 
 
 class Creature
 {
-private:
+protected:
     std::string m_name{};
     char m_symbol{};
-    int m_hp{};
-    int m_attack{};
-    int m_gold_lot{};
+    int m_health{};
+    int m_damage{};
+    int m_gold{};
 
 public:
-    Creature(const std::string& name, char symbol, int hp, int attack, int gold_lot)
-        : m_name{ name }, m_symbol{ symbol }, m_hp{ hp }, m_attack{ attack }, m_gold_lot{ gold_lot }
-    {
-    }
+    Creature(std::string_view name, char symbol, int health, int damage, int gold)
+		: m_name{ name }
+		, m_symbol{ symbol }
+		, m_health{ health }
+		, m_damage{ damage }
+		, m_gold{ gold }
+	{
+	}
 
     const std::string& getName() const { return m_name; }
     char getSymbol() const { return m_symbol; }
-    int getHp() const { return m_hp; }
-    int getAttack() const { return m_attack; }
-    int getGold_lot() const { return m_gold_lot; }
+    int getHp() const { return m_health; }
+    int getAttack() const { return m_damage; }
+    int getGold() const { return m_gold; }
 
-    void reduceHealth(int hp)
-    {
-        m_hp -= hp;
-    }
-
-    bool isDead(int hp)
-    {   
-        if(hp <= 0)
-        {
-            return true;
-        }
-    }
-
-    void addGold(int gold)
-    {
-        m_gold_lot += gold;
-    }
-
+    void reduceHealth(int hp){ m_health -= hp; }
+    bool isDead() const { return m_health <= 0; }
+    void addGold(int gold){ m_gold += gold; }
 };
-//a
+
 class Player: public Creature
 {
+private:
+    int m_lvl{ 1 };
+
 public:
-    Player( const std::string& name, char symbol, int hp, int attack, int gold_lot )
-        : Creature{ name, symbol, hp, attack, gold_lot }
-    {
+    Player(std::string_view name)
+        : Creature{ name, '@', 10, 1, 0 }
+    { 
     }
 
+    void levelUp()
+    {
+       ++m_lvl;
+       ++m_damage; 
+    }
+
+    int getLvl() const { return m_lvl; }
+    bool hasWon() const { return m_lvl >= 20; }
+
+    friend std::ostream& operator<<(std::ostream& out, const Player& player)
+    {
+        out << "Welcome, " << player.getName() << ".\n";
+        out << "You have " << player.getHp() << " health and are carrying " << player.getGold() << "  gold.\n";
+        return out;
+    }
 };
 
 class Monster: public Creature
 {
+public:
+    
+    enum class Type
+    {
+        type_dragon,
+        type_orc, 
+        type_slime,
+        max_types,
+    };  
+
+
+
 
 };
 
+std::string ask_for_User_name()
+{
+    std::string name{};
+    std::cout << "Enter your name: ";
+    std::cin >> name;
+
+    return name;
+}
+
 int main()
 {
+    //check C++ version:
+    if (__cplusplus == 201703L) std::cout << "C++17\n";
+    else if (__cplusplus == 201402L) std::cout << "C++14\n";
+    else if (__cplusplus == 201103L) std::cout << "C++11\n";
+    else if (__cplusplus == 199711L) std::cout << "C++98\n";
+    else std::cout << "pre-standard C++\n";
+
+
     Creature o{ "orc", 'o', 4, 2, 10 };
     o.addGold(5);
     o.reduceHealth(1);
 
-    std::cout << 
+    std::cout << "The " << o.getName() << " has " << o.getHp() << " health and is carrying " << o.getGold() << " gold.\n";
+
+    Player player1{ ask_for_User_name() };
+    std::cout << player1;
+
 
 
     return 0;
